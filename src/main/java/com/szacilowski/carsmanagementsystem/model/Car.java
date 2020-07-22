de3c083e3c0aa7c0bd912f7cbcdb3b2c202b1f03
piotrszacilowski <szacilowski.piotr@gmail.com>
2020-07-22 13:44:21 +0200

    Create Car model

--- src/main/java/com/szacilowski/carsmanagementsystem/model/Car.java
+++ src/main/java/com/szacilowski/carsmanagementsystem/model/Car.java
@@ -1,4 +1,10 @@
 package com.szacilowski.carsmanagementsystem.model;
 
+import lombok.Data;
+
+@Data
 public class Car {
+    private String brand;
+    private String model;
+    private int year;
 }
