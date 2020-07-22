0642c92bb5c8bc13a3afb292e1c723c2659d5e96
piotrszacilowski <szacilowski.piotr@gmail.com>
2020-07-22 13:26:07 +0200

    initial commit

---
+++ src/main/java/com/szacilowski/carsmanagementsystem/CarsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.szacilowski.carsmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/szacilowski/carsmanagementsystem/CarsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szacilowski.carsmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
