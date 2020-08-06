345771f2e004e44ade8485a8adc866078771844b
Piotr Szaciłowski <szacilowski.piotr@gmail.com>
2020-08-06 13:36:39 +0200

    Change @GeneratedValue to Identity strategy

--- src/main/java/com/szacilowski/carsmanagementsystem/model/Car.java
+++ src/main/java/com/szacilowski/carsmanagementsystem/model/Car.java
@@ -17,7 +17,7 @@ import javax.persistence.Id;
 public class Car {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String brand;
     private String model;
